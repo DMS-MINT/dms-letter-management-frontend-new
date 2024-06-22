61aac7d4f1e95f34068098300d28528a1769f52b
absra47 <absragirma630@gmail.com>
2024-06-22 20:07:48 +0300

    fun: passing the permission on formdata

--- components/features/letter/forms/ShareLetterForm.tsx
+++ components/features/letter/forms/ShareLetterForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Select as SelectUI,
   SelectContent,
@@ -32,53 +31,36 @@ import { Label } from "@/components/ui/label";
 import { shareLetter } from "@/lib/features/letter/workflow/workflowSlice";
 import { selectLetterDetails } from "@/lib/features/letter/letterSlice";
 import React from "react";
-import { Separator } from "@radix-ui/react-select";
-import { ArrowLeft } from "lucide-react";
+import { ParticipantRolesEnum } from "@/typing/enum";
 
-type Privilage = {
+type Privilege = {
   value: string;
   label: string;
 };
 
-const privilages: Privilage[] = [
-  {
-    value: "can_view_letter",
-    label: "ተመልካች",
-  },
-  {
-    value: "can_comment_letter",
-    label: "አስተያየት ሰጪ",
-  },
-  {
-    value: "can_update_letter",
-    label: "አርታዒ",
-  },
-  {
-    value: "can_share_letter",
-    label: "አጋሪ",
-  },
-  {
-    value: "transfer_ownership",
-    label: "ባለቤትነትን ያስተላልፉ",
-  },
-  {
-    value: "remove_access",
-    label: "ፈቃድን ያስወግዱ",
-  },
+const privileges: Privilege[] = [
+  { value: "can_view_letter", label: "ተመልካች" },
+  { value: "can_comment_letter", label: "አስተያየት ሰጪ" },
+  { value: "can_update_letter", label: "አርታዒ" },
+  { value: "can_share_letter", label: "አጋሪ" },
+  { value: "transfer_ownership", label: "ባለቤትነትን ያስተላልፉ" },
+  { value: "remove_access", label: "ፈቃድን ያስወግዱ" },
 ];
 
 interface IFormData {
-  to: string;
+  to: Array<string>;
   message: string;
+  permission: Array<string>;
 }
 
 export default function ShareLetterForm() {
-  const [selectedPrivilage, setSelectedPrivilage] = useState(null);
+  const [selectedPrivilage, setSelectedPrivilage] = useState<string[]>([]);
   const [open, setOpen] = useState(false);
 
   const [formData, setFormData] = useState<IFormData>({
-    to: "",
+    to: [],
     message: "",
+    permission: [],
   });
   const contacts = useAppSelector(selectContacts);
   const [options, setOptions] = useState<IOption[]>([]);
@@ -98,17 +80,42 @@ export default function ShareLetterForm() {
     }
   }, [contacts]);
 
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
   const handleSelectChange = (
     option: SingleValue<IOption> | MultiValue<IOption>,
     actionMeta: ActionMeta<IOption>
   ) => {
-    const { name, option: selectedOption } = actionMeta;
+    const selectedOptions = Array.isArray(option) ? option : [option];
+    const to = selectedOptions.map(
+      (selectedOption) => selectedOption?.value as string
+    );
 
-    const to = selectedOption?.value as string;
-
-    setFormData((prevData: IFormData) => ({ ...prevData, to }));
+    setFormData((prevData: IFormData) => ({
+      ...prevData,
+      to,
+    }));
   };
 
+  const handlePrivilegeChange = (value: string) => {
+    setSelectedPrivilage((prevPrivileges) => {
+      let newPrivileges;
+      if (prevPrivileges.includes(value)) {
+        newPrivileges = prevPrivileges.filter((priv) => priv !== value);
+      } else {
+        newPrivileges = [...prevPrivileges, value];
+      }
+
+      setFormData((prevData: IFormData) => ({
+        ...prevData,
+        permission: newPrivileges,
+      }));
+
+      return newPrivileges;
+    });
+  };
   const handleMessageChange = (message: string) => {
     setFormData((prevData: IFormData) => ({ ...prevData, message }));
   };
@@ -120,6 +127,13 @@ export default function ShareLetterForm() {
         participant: formData,
       })
     );
+  };
+
+  const getLabelForValue = (value: string) => {
+    const selectedPrivilege = privileges.find(
+      (privilege) => privilege.value === value
+    );
+    return selectedPrivilege ? selectedPrivilege.label : "Select";
   };
 
   return (
@@ -140,25 +154,29 @@ export default function ShareLetterForm() {
               placeholder=""
               className="w-full"
             />
-            {formData.to ? (
-              <SelectUI defaultValue="can_view_letter">
-                <SelectTrigger
-                  onClick={() => console.log("dsfs")}
-                  className="ml-auto w-[165px]"
-                >
-                  <SelectValue placeholder="Select" />
+            {formData.to.length > 0 ? (
+              <SelectUI
+                value={selectedPrivilage.join(",")}
+                onValueChange={handlePrivilegeChange}
+              >
+                <SelectTrigger className="ml-auto w-[165px]">
+                  <SelectValue>
+                    {getLabelForValue(
+                      selectedPrivilage[selectedPrivilage.length - 1]
+                    )}
+                  </SelectValue>
                 </SelectTrigger>
                 <SelectContent>
-                  {privilages.map((privilage) => (
-                    <SelectItem key={privilage.value} value={privilage.value}>
-                      {privilage.label}
+                  {privileges.map((privilege) => (
+                    <SelectItem key={privilege.value} value={privilege.value}>
+                      {privilege.label}
                     </SelectItem>
                   ))}
                 </SelectContent>
               </SelectUI>
             ) : null}
           </div>
-          {formData.to ? (
+          {formData.to.length > 0 ? (
             <div className="flex flex-col gap-2 mt-4">
               <Label>መልእክት ማስቀመጫ</Label>
               <Textarea
@@ -192,39 +210,61 @@ export default function ShareLetterForm() {
                         </p>
                       </div>
                     </div>
+
                     <p>ባለቤት</p>
                   </div>
-                  <div className="flex items-center justify-between space-x-4">
-                    <div className="flex items-center space-x-4">
-                      <Avatar>
-                        <AvatarImage src="/avatars/05.png" />
-                        <AvatarFallback>ቤበ</AvatarFallback>
-                      </Avatar>
-                      <div>
-                        <p className="text-sm font-medium leading-none">
-                          ሚኒስትር ጽ/ቤት ኃላፊ
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          ቤዛዊት በጋሻው
-                        </p>
-                      </div>
-                    </div>
-                    <SelectUI defaultValue="can_view_letter">
-                      <SelectTrigger className="ml-auto w-[165px]">
-                        <SelectValue placeholder="Select" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {privilages.map((privilage) => (
-                          <SelectItem
-                            key={privilage.value}
-                            value={privilage.value}
-                          >
-                            {privilage.label}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </SelectUI>
-                  </div>
+                  {letterDetails.participants.length > 0
+                    ? letterDetails.participants
+                        .filter(
+                          (participant) =>
+                            participant.role_name ===
+                            ParticipantRolesEnum["BLIND CARBON COPY RECIPIENT"]
+                        )
+                        .map(({ id, user }) => (
+                          <div className="flex items-center justify-between space-x-4">
+                            <div className="flex items-center space-x-4">
+                              <Avatar>
+                                <AvatarImage src="/avatars/05.png" />
+                                <AvatarFallback>ቤበ</AvatarFallback>
+                              </Avatar>
+                              <div>
+                                <p className="text-sm font-medium leading-none">
+                                  ሚኒስትር ጽ/ቤት ኃላፊ
+                                </p>
+                                <p className="text-sm text-muted-foreground">
+                                  ቤዛዊት በጋሻው
+                                </p>
+                              </div>
+                            </div>
+                            <SelectUI
+                              value={selectedPrivilage.join(",")}
+                              onValueChange={handlePrivilegeChange}
+                            >
+                              <SelectTrigger className="ml-auto w-[165px]">
+                                <SelectValue>
+                                  {selectedPrivilage.length > 0
+                                    ? getLabelForValue(
+                                        selectedPrivilage[
+                                          selectedPrivilage.length - 1
+                                        ]
+                                      )
+                                    : "Select"}
+                                </SelectValue>
+                              </SelectTrigger>
+                              <SelectContent>
+                                {privileges.map((privilege) => (
+                                  <SelectItem
+                                    key={privilege.value}
+                                    value={privilege.value}
+                                  >
+                                    {privilege.label}
+                                  </SelectItem>
+                                ))}
+                              </SelectContent>
+                            </SelectUI>
+                          </div>
+                        ))
+                    : null}
                   <div className="flex items-center justify-between space-x-4">
                     <div className="flex items-center space-x-4">
                       <Avatar>
@@ -240,17 +280,26 @@ export default function ShareLetterForm() {
                         </p>
                       </div>
                     </div>
-                    <SelectUI defaultValue="can_view_letter">
+                    <SelectUI
+                      value={selectedPrivilage.join(",")}
+                      onValueChange={handlePrivilegeChange}
+                    >
                       <SelectTrigger className="ml-auto w-[165px]">
-                        <SelectValue placeholder="Select" />
+                        <SelectValue>
+                          {selectedPrivilage.length > 0
+                            ? getLabelForValue(
+                                selectedPrivilage[selectedPrivilage.length - 1]
+                              )
+                            : "Select"}
+                        </SelectValue>
                       </SelectTrigger>
                       <SelectContent>
-                        {privilages.map((privilage) => (
+                        {privileges.map((privilege) => (
                           <SelectItem
-                            key={privilage.value}
-                            value={privilage.value}
+                            key={privilege.value}
+                            value={privilege.value}
                           >
-                            {privilage.label}
+                            {privilege.label}
                           </SelectItem>
                         ))}
                       </SelectContent>
